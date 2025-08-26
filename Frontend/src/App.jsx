559fcf128e0ea015e51e8e11ff18e8044b61b25a
HEMA CHANDRIKA D <chandrikah714@gmail.com>
2025-08-27 00:28:08 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,9 @@
-import { Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Profile from "./pages/Profile";
-import ProfileForm from "./pages/ProfileForm";
+import { RouterProvider } from 'react-router-dom'
+import Routes from './routes/Routes'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      
-      <Route path="/profile" element={<Profile/>} />
-      <Route path="/profileForm" element={<ProfileForm/>} />
-
-    </Routes>
+    <RouterProvider router={Routes}/>
   );
 }
 
